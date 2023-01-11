44bf058e81ff4d535b57f9b044735f71253e7f4e
nabom921126 <nabom921126@gmail.com>
2023-01-11 10:41:56 +0900

    20230111 오하영

--- hayoung/dodoWorld.js
+++ hayoung/dodoWorld.js
@@ -25,10 +25,10 @@ searchBtn.onclick = function(){
     for(var i = 0; i < arr.length; i++){
         if(search.value == arr[i]){
             if(i < 8){
-                window.location.href = "http://127.0.0.1:5500/Search_dog.html";
+                window.location.href = "http://127.0.0.1:5500/hayoung/Search_dog.html";
                 search.value = "";
             }else if( 7 < i){
-                window.location.href = "http://127.0.0.1:5500/Search_cat.html"
+                window.location.href = "http://127.0.0.1:5500/hayoung/Search_cat.html"
                 search.value = "";
             }
             
